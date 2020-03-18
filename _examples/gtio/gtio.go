15135f6d41d472859683a54ca00bc5194e433150
mo <thinkgo@aliyun.com>
2020-03-18 22:43:12 +0800

    update example

---
+++ _examples/gtio/gtio.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"time"
+
+	"github.com/thinkgos/gt65/gtio"
+)
+
+func main() {
+	led, err := gtio.OpenIO("/dev/led")
+	if err != nil {
+		panic(err)
+
+	}
+
+	for {
+		led.On()
+		time.Sleep(time.Second * 1)
+		led.Off()
+		time.Sleep(time.Second * 1)
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/thinkgos/gt65
 
 go 1.14
 
-require github.com/go-ini/ini v1.55.0
+require (
+	github.com/go-ini/ini v1.55.0
+	github.com/smartystreets/goconvey v1.6.4 // indirect
+	gopkg.in/ini.v1 v1.55.0 // indirect
+)
